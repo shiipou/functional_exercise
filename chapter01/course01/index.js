2c18a735dfac35466f33de5ea1503fdb069ddad1
antonin <opm852159@gmail.com>
2021-11-02 10:35:42 +0100

    on a return le text en brute puis on mes la premiere lettre puis tous en minuscule

--- chapter01/course01/index.js
+++ chapter01/course01/index.js
@@ -1,13 +1,18 @@
 
 function greetWorld() {
-
+return 'Hey, world!'
 }
 
 function greet(name) {
+  return `Hey, ${name}!`
 }
 
 function capitilizedGreet(name) {
-
+  const firstLetter = name.charAt(0)
+  let res = firstLetter.toUpperCase()
+  const otherletter = name.substring(1)
+   res += otherletter.toLowerCase()
+   return greet(res)
 }
 
 module.exports = {
